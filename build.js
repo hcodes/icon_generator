c82e05e74d947b8a3d69951aedc850316adfe0ba
Denis Seleznev <hcodes@yandex.ru>
2013-11-12 12:50:47 +0400

    Иконки источников трафика

--- build.js
+++ build.js
@@ -3,21 +3,18 @@
 var fs = require('fs');
 var http = require('http');
 var util = require('util');
-const TAB = '    ';
+var template = fs.readFileSync('./src/template.html', {encoding: 'utf8'});
 
+const TAB = '    ';
+const COMMENT = '/* Файл сгенерирован с помощью icon_generator */';
 const FAVICON_SERVICE = 'http://favicon.yandex.net/favicon/';
 
-var template = fs.readFileSync('./src/template.html', {encoding: 'utf8'});
     
 function getFavicon(url) {
 }    
 
 function loadJSON(fileName) {
     return JSON.parse(fs.readFileSync(fileName));
-}
-
-function autocomment() {
-    return '/* Файл сгенерирован с помощью tools/icon_generator/ */';
 }
 
 function getCSSClass(prefix, id) {
@@ -42,18 +39,22 @@ function buildCSSElement(cssPrefix, name, id, file) {
 }
 
 function buildType(data) {
-    var text = autocomment() + '\n\n';
+    var text = COMMENT + '\n\n';
     
     var base = loadJSON('./types/' + data.id + '.json');
     text += data.cssPrefix + '\n' + fs.readFileSync('./types/' + data.id + '.css') + '\n';
     
     base.forEach(function (el, i) {
         var image = './images/' + data.id + '/' + (data.fileNameFromName ? el.name : el.id) + '.png';
+        var url = '';
+        if (el.url) {
+            url += ' <a href="' + el.url + '" target="_blank">' + el.url + '</a>'; 
+        }
         text += buildCSSElement(data.cssPrefix, el.name, el.id, image);
-        content.push('<li><i class="' + data.cssClass + ' ' + getCSSClass(data.cssClass, el.id) + '"></i> ' + el.name + '</li>');
+        content.push('<li><i class="' + data.cssClass + ' ' + getCSSClass(data.cssClass, el.id) + '"></i> ' + el.name + ' (' + el.id + url + ')</li>');
     }, this);
     
-    text += '\n\n' + autocomment();
+    text += '\n\n' + COMMENT;
     
     fs.writeFileSync(data.fileName, text);
 }
@@ -68,7 +69,11 @@ function onEnd() {
 
 var types = loadJSON('types.json');
 types.sort(function (a, b) {
-    return a.id > b.id;
+    if (a.title > b.title) {
+        return 1;
+    }
+    
+    return -1;
 });
 
 var link = [];
@@ -76,7 +81,7 @@ var content = [];
 
 content.push('<ul id="menu">');
 types.forEach(function (el, i) {
-    content.push('<li><a href="#' + el.id + '">' + el.id + '</a></li>');
+    content.push('<li><a href="#' + el.id + '">' + el.title + '</a></li>');
 });
 
 content.push('</ul><br /><br />');
@@ -84,10 +89,10 @@ content.push('</ul><br /><br />');
 types.forEach(function (el, i) {
     link.push('<link rel="stylesheet" type="text/css" href="' + el.fileName + '?rnd=' + (~~(Math.random * 1E10)) + '" />');
     
-    content.push('<h1 id="' + el.id + '">' + el.id + '</h1>');
+    content.push('<h1 id="' + el.id + '">' + el.description + '</h1>');
     content.push('<ul>');
     
-    console.log(i + '..' + types.length + ' - ' + el.id);
+    console.log((i + 1) + '/' + types.length + ' - ' + el.id);
     
     buildType(el);
     
